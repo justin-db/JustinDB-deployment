bcf469c986dafa025c96209e286b1175b3fb6696
speedcom <mateusz.maciaszekhpc@gmail.com>
2017-10-02 07:46:45 +0200

    update script

--- docker/local/docker-compose-up.sh
+++ docker/local/docker-compose-up.sh
@@ -11,12 +11,12 @@ docker run \
 
 docker run \
     --name justindb-1 -d \
-    -p 9000:9000 -p 2551:2551 \
+    -p 9000:9000 -p 2551:2551 -p 19999:19999 \
     -v justindb-1-vol:/opt/docker/justindb \
     justindb/justindb:0.1 \
     -Djustin.node-id=0 \
-    -Djustin.storage-type=justin.db.storage.PersistentStorage \
-    -Djustin.storage-journal-path=/opt/docker/justindb/journal \
+    -Djustin.storage-type=justin.db.storage.RocksDBStorage \
+    -Djustin.storage-journal-path=/opt/docker/justindb/rocksdb \
     -Dakka.remote.netty.tcp.hostname=192.168.0.3 \
     -Dakka.remote.netty.tcp.port=2551 \
     -Dakka.remote.netty.tcp.bind-hostname=172.17.0.3 \
@@ -25,12 +25,12 @@ docker run \
 
 docker run \
     --name justindb-2 -d \
-    -p 9001:9000 -p 2552:2552 \
+    -p 9001:9000 -p 2552:2552 -p 20000:19999 \
     -v justindb-2-vol:/opt/docker/justindb \
     justindb/justindb:0.1 \
     -Djustin.node-id=1 \
-    -Djustin.storage-type=justin.db.storage.PersistentStorage \
-    -Djustin.storage-journal-path=/opt/docker/justindb/journal \
+    -Djustin.storage-type=justin.db.storage.RocksDBStorage \
+    -Djustin.storage-journal-path=/opt/docker/justindb/rocksdb \
     -Dakka.remote.netty.tcp.hostname=192.168.0.3 \
     -Dakka.remote.netty.tcp.port=2552 \
     -Dakka.remote.netty.tcp.bind-hostname=172.17.0.4 \
@@ -39,12 +39,12 @@ docker run \
 
 docker run \
     --name justindb-3 -d \
-    -p 9002:9000 -p 2553:2553 \
+    -p 9002:9000 -p 2553:2553 -p 20001:19999 \
     -v justindb-3-vol:/opt/docker/justindb \
     justindb/justindb:0.1 \
     -Djustin.node-id=2 \
-    -Djustin.storage-type=justin.db.storage.PersistentStorage \
-    -Djustin.storage-journal-path=/opt/docker/justindb/journal \
+    -Djustin.storage-type=justin.db.storage.RocksDBStorage \
+    -Djustin.storage-journal-path=/opt/docker/justindb/rocksdb \
     -Dakka.remote.netty.tcp.hostname=192.168.0.3 \
     -Dakka.remote.netty.tcp.port=2553 \
     -Dakka.remote.netty.tcp.bind-hostname=172.17.0.5 \
@@ -57,4 +57,4 @@ curl 192.168.0.3:9000/health
 curl 192.168.0.3:9001/health
 curl 192.168.0.3:9002/health
 
-curl 192.168.0.3:9000/cluster
+curl 192.168.0.3:19999/members
